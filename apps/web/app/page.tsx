141ac4dc92f7bce3714a4535f43271dd42214608
Unfortunated <thanyathornwiengperm@gmail.com>
2024-10-07 13:47:28 +0700

    Google login

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
-        <div className='grid min-h-dvh w-full place-content-center'>
-            <p className='text-center text-2xl'>Find My Mines</p>
+        <div className='grid min-h-dvh w-full place-content-center' >
+            <p className='text-center text-2xl' style={background-color:"black;"}>Find My Mines</p>
+            <p>Find the mines before they find you!</p>
         </div>
     );
 }
