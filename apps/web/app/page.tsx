07cc107223ffa47b073c9023afd4b4db15b24bda
Unfortunated <thanyathornwiengperm@gmail.com>
2024-10-09 14:56:09 +0700

    homepage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,12 +7,41 @@ export default function Home() {
             className='flex grid min-h-dvh w-full flex-grow place-content-center items-center justify-center gap-5'
             style={{ backgroundColor: '#0D1321' }}
         >
-            <div className='font-Montserrat mb-15 text-center text-7xl font-bold'>
-                <span className='text-[#FFEDDF]'>Find My </span>
-                <span className='text-[#C33432]'>Mines</span>
+            <div className='mb-12'>
+                <div className='items-center'>
+                    <Button
+                        variant='default'
+                        color='gray'
+                        size='lg'
+                        className='absolute left-20 text-left'
+                    >
+                        <img src='shop.svg' className='h-12 w-12 pr-4'></img>
+                        Shop
+                    </Button>
+                </div>
+                <div className='items-center'>
+                    <Button
+                        variant='default'
+                        color='gray'
+                        size='lg'
+                        className='absolute right-20 text-right'
+                    >
+                        <img
+                            src='scoreboard.svg'
+                            className='h-12 w-12 pr-4'
+                        ></img>
+                        Scoreboard
+                    </Button>
+                </div>
             </div>
-            <div className='font-Montserrat mb-15 text-center text-base font-bold text-[#C59CC8]'>
-                Find the mines before they find you!
+            <div className='mb-20'>
+                <div className='font-Montserrat text-center text-7xl font-bold'>
+                    <span className='text-[#FFEDDF]'>Find My </span>
+                    <span className='text-[#C33432]'>Mines</span>
+                </div>
+                <div className='font-Montserrat text-center text-base font-bold text-[#C59CC8]'>
+                    Find the mines before they find you!
+                </div>
             </div>
             <div className='flex flex-row justify-center'>
                 <img
@@ -24,7 +53,7 @@ export default function Home() {
                         Log in with{' '}
                         <img
                             src='https://techdocs.akamai.com/identity-cloud/img/social-login/identity-providers/iconfinder-new-google-favicon-682665.png'
-                            className='ml-2 h-5 w-5 items-center'
+                            className='ml-2 h-8 w-8 items-center'
                         ></img>
                     </Button>
                 </div>
@@ -46,6 +75,41 @@ export default function Home() {
                 <Button variant='default' color='green' size='lg'>
                     Play
                 </Button>
+            </div>
+            <div className='mt-20 flex flex-row items-center justify-center'>
+                <div>
+                    <Button
+                        variant='default'
+                        color='gray'
+                        size='lg'
+                        className='absolute left-20 text-left'
+                    >
+                        <img src='skin.svg' className='h-16 w-16 pr-4'></img>
+                        My Skins
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        variant='default'
+                        color='gray'
+                        size='lg'
+                        className='text-center'
+                    >
+                        <img src='circle-help.svg' className='pr-4'></img>
+                        Help
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        variant='default'
+                        color='gray'
+                        size='lg'
+                        className='absolute right-20 text-right'
+                    >
+                        <img src='earth.svg' className='h-12 w-12 pr-4'></img>
+                        ENG
+                    </Button>
+                </div>
             </div>
         </div>
     );
