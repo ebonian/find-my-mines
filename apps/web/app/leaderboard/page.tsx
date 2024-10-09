107b7dc06b47de7f175ecf07884b460e7ef54637
KrittapopManupornprapa <krittapop.manupornprapa@safecoms.asia>
2024-10-09 15:36:26 +0700

    forgor page

--- apps/web/app/leaderboard/page.tsx
+++ apps/web/app/leaderboard/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from "react";
-import BackButton from "../components/ui/BackButton";
+import BackButton from "../_components/ui/BackButton";
 
 export default function leaderboard() {
     type returnMyData = {
