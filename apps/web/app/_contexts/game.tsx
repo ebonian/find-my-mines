8f5d90ae4a5873517b7252ff31dfed6087ab56c4
TungDude <krittapop.kms@gmail.com>
2024-10-25 15:33:15 +0700

    seed gen only god knows if it works or not

--- apps/web/app/_contexts/game.tsx
+++ apps/web/app/_contexts/game.tsx
@@ -26,9 +26,34 @@ export default function GameContextProvider({
     // STATES
     const [gameRooms, setGameRooms] = useState<Room[]>([]);
 
+    // Seed Generation
+    const seedGen = () => {
+        const replaceAt = (oStr: String, index: number, replacement: String) => {
+            return oStr.substring(0, index) + replacement + oStr.substring(index + replacement.length);
+        }
+
+        var numSeq = Math.floor(Math.random() * (999999999 - 100000000) + 100000000).toString();
+
+        for (let i = 0; i < 5; i++) {
+            var pos = Math.floor(Math.random() * 8);
+            var dec = Math.floor(Math.random() * (126 - 58) + 58);
+
+            while (true) {
+                if (!isNaN(parseInt(numSeq.charAt(pos), 10))) {
+                    numSeq = replaceAt(numSeq, pos % 9, String.fromCharCode(dec));
+                    break;
+                }
+                pos += 1
+            }
+        }
+        return { "seed": numSeq };
+    }
+
     // METHODS
     const createRoom = (room: Room) => {
-        send('create-room', room);
+        const seed = seedGen();
+        const roomWithSeed = {...room, seed};
+        send('create-room', roomWithSeed);
     };
 
     // EFFECTS
