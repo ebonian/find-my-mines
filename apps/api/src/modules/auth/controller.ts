428fc884bb22e0f4118d2518bdbb31a586ce0535
ebonian <tanadon.santisan@gmail.com>
2024-10-26 22:58:03 +0700

    feat: auth routes

--- apps/api/src/modules/auth/controller.ts
+++ apps/api/src/modules/auth/controller.ts
@@ -3,6 +3,23 @@ import passport from 'passport';
 
 const router = Router();
 
+router.get('/auth/me', (req, res) => {
+    if (req.user) {
+        res.json(req.user);
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+router.post('/auth/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return res.status(500).send;
+        }
+    });
+    res.sendStatus(200);
+});
+
 router.get('/auth/google', async (req, res, next) => {
     passport.authenticate('google', { scope: ['email', 'profile'] })(
         req,
